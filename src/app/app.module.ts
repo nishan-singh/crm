0ce30a7e1e7f4c355cd86217d67e4beac37f5c26
Nishan Singh <singh007nishan@gmail.com>
2023-07-25 19:17:45 +0200

    added footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavSidebarComponent } from './components/nav-sidebar/nav-sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NavSidebarComponent],
+  declarations: [AppComponent, NavSidebarComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
