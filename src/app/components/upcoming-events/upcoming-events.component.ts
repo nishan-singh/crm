6c756fc4faed5fff5b9fcb43c8251c73727778c4
Nishan Singh <singh007nishan@gmail.com>
2023-07-26 14:55:00 +0200

    styled upcoming events component

--- src/app/components/upcoming-events/upcoming-events.component.ts
+++ src/app/components/upcoming-events/upcoming-events.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-upcoming-events',
   templateUrl: './upcoming-events.component.html',
-  styleUrls: ['./upcoming-events.component.scss']
+  styleUrls: ['./upcoming-events.component.scss'],
 })
 export class UpcomingEventsComponent {
+  isFullScreen: boolean = false;
 
+  @HostListener('document: keydown', ['$event'])
+  onKeydownHandler(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      this.isFullScreen = false;
+    }
+  }
+
+  toggleFullScreen(): void {
+    this.isFullScreen = !this.isFullScreen;
+  }
 }
