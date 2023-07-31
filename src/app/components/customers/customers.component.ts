7ff5244fa8891fbdcff42d975d71dce09188deb9
Nishan Singh <singh007nishan@gmail.com>
2023-07-31 20:04:36 +0200

    added customers

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-customers',
   templateUrl: './customers.component.html',
-  styleUrls: ['./customers.component.scss']
+  styleUrls: ['./customers.component.scss'],
 })
 export class CustomersComponent {
+  private firestore: Firestore = inject(Firestore);
+  isFullScreen: boolean = false;
 
+  customers$: Observable<any[]> = collectionData(
+    collection(this.firestore, 'customers')
+  );
+
+  constructor() {}
+
+  @HostListener('document: keydown', ['$event'])
+  onKeydownHandler(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      document.body.style.overflow = 'auto';
+      this.isFullScreen = false;
+    }
+  }
+
+  toggleFullScreen(): void {
+    document.body.style.overflow = this.isFullScreen ? 'auto' : 'hidden';
+    this.isFullScreen = !this.isFullScreen;
+  }
 }
