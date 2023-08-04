a46ff994038244960d6fb1952b9736939c199403
Nishan Singh <singh007nishan@gmail.com>
2023-08-04 21:59:18 +0200

    working on implementing delete function

--- src/app/components/customers/customers.component.ts
+++ src/app/components/customers/customers.component.ts
@@ -7,6 +7,7 @@ import {
   getDocs,
   doc,
   deleteDoc,
+  addDoc,
 } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
@@ -24,7 +25,13 @@ export class CustomersComponent {
     collection(this.firestore, 'customers')
   );
 
-  constructor() {}
+  constructor() {
+    this.addCustomer({
+      name: 'John Doe',
+      age: 30,
+      address: '1234 Main St.',
+    });
+  }
 
   @HostListener('document: keydown', ['$event'])
   onKeydownHandler(event: KeyboardEvent): void {
@@ -32,6 +39,12 @@ export class CustomersComponent {
       document.body.style.overflow = 'auto';
       this.isFullScreen = false;
     }
+  }
+
+  async addCustomer(customerData: any): Promise<string> {
+    const collectionRef = collection(this.firestore, 'customers');
+    console.log('Document written with ID: ', collectionRef.id);
+    return collectionRef.id;
   }
 
   toggleFullScreen(): void {
