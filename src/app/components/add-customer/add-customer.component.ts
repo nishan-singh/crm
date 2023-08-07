f4675d9784cb0a72bceabd94d3fba25f203be17a
Nishan Singh <singh007nishan@gmail.com>
2023-08-07 17:32:25 +0200

    added add customer component

---
+++ src/app/components/add-customer/add-customer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-add-customer',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './add-customer.component.html',
+  styleUrls: ['./add-customer.component.scss']
+})
+export class AddCustomerComponent {
+
+}
