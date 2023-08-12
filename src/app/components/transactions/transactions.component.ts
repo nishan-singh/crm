12599cc5891b6a7e939900ea3429d0e9344337b5
Nishan Singh <singh007nishan@gmail.com>
2023-08-12 10:43:46 +0200

    added transactions from database

--- src/app/components/transactions/transactions.component.ts
+++ src/app/components/transactions/transactions.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, HostListener } from '@angular/core';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  query,
+  orderBy,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
-  styleUrls: ['./transactions.component.scss']
+  styleUrls: ['./transactions.component.scss'],
 })
 export class TransactionsComponent {
+  private firestore: Firestore = inject(Firestore);
+  isFullScreen: boolean = false;
 
+  transactions$: Observable<any[]>;
+
+  constructor() {
+    const order = orderBy('date');
+    const upcomingEventsRef = collection(this.firestore, 'transactions');
+    const upcomingEventsQuery = query(upcomingEventsRef, order);
+    this.transactions$ = collectionData(upcomingEventsQuery) as Observable<
+      any[]
+    >;
+  }
+
+  @HostListener('document: keydown', ['$event'])
+  onKeydownHandler(event: KeyboardEvent): void {
+    if (event.key === 'Escape') {
+      document.body.style.overflow = 'auto';
+      this.isFullScreen = false;
+    }
+  }
+
+  toggleFullScreen(): void {
+    document.body.style.overflow = this.isFullScreen ? 'auto' : 'hidden';
+    this.isFullScreen = !this.isFullScreen;
+  }
 }
